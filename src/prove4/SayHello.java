87f92e4e1cd3bfbfbb3ced53ead21d2461a2cbea
Isaac Rodriguez <64040285+rdzisaac23@users.noreply.github.com>
2020-10-07 11:31:27 -0500

    Initial commit

---
+++ src/prove4/SayHello.java
@@ -0,0 +1,14 @@
+package prove4;
+
+public class SayHello {
+
+    static void helloIsaac(){
+        System.out.println("Hello, my name is Isaac");
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        SayHello hello = new SayHello();
+        hello.helloIsaac();
+    }
+}
